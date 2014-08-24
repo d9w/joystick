a297490580fead66bce823fd52529e7cef59c659
Dennis Wilson <dennisw@infinidat.com>
2014-08-24 10:34:44 +0300

    button tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,13 +1,64 @@
 from tests import TestCase
 from joystick.extensions import db
-from joystick.models import Command
+from joystick.models import Command, ButtonCommand
+from time import sleep
 
-class TestCommand(TestCase):
+class TestButtons(TestCase):
 
-    def test_make_command(self):
+    def test_button_create(self):
 
-        command = Command()
-        db.session.add(command)
+        button = ButtonCommand()
+        db.session.add(button)
         db.session.commit()
+        assert button in db.session
 
-        assert command in db.session
+    def test_button_log(self):
+
+        button = ButtonCommand(cmd='uptime')
+        button.press()
+        assert 'load' in button.get_log()
+
+    def test_button_log_summary(self):
+
+        button = ButtonCommand(cmd='for ((i=0; i<10; i++)); do echo $i; done')
+        button.press()
+        assert '9' in button.get_log_tail(5)
+        assert '1' not in button.get_log_tail(5)
+
+    def test_button_update(self):
+
+        button = ButtonCommand(cmd='uptime')
+        button.press()
+        assert 'load' in button.get_log()
+        button.cmd = 'ip addr'
+        button.press()
+        assert 'link' in button.get_log()
+
+    def test_button_lock(self):
+
+        button = ButtonCommand(cmd='sleep 3')
+        button.press()
+        assert button.locked
+        sleep(4)
+        assert not button.locked
+
+    def test_button_delete(self):
+
+        button = ButtonCommand(cmd='ping www.google.com -i 0.2')
+        button.press()
+        db.session.add(button)
+
+        log_file = button.log_file
+        assert open(log_file, 'r').read()
+
+        db.session.delete(button)
+
+        assert button not in db.session
+
+        sleep(1) # to make sure the process is killed and not pinging anymore
+
+        try:
+            open(log_file, 'r')
+        except IOError:
+            assert True
+        #self.assertRaises(IOError, open(log_file))
